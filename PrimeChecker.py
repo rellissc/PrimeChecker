4b3bf3f1509e38b2e851deb24d364ffd51636497
Robert Ellis <rellissc@gmail.com>
2020-03-23 00:28:11 -0400

    Added parsing of manufacturing requirements, and beginning framework of UI

--- PrimeChecker.py
+++ PrimeChecker.py
@@ -1,19 +1,102 @@
 
 import bs4 as BS
 import urllib.request
-from PrimeItem import PrimeItem
+#import PrimeItem
+from PrimeItem import PrimeBlueprint, PrimePart
 from tkinter import *
+from tkinter import ttk
+import json
 
 
-source = urllib.request.urlopen('https://warframe.fandom.com/wiki/Prime').read()
-soup=BS.BeautifulSoup(source,'html.parser')
-testDiv=soup.find('div', id='gallery-0')
-x=PrimeItem()
-gal0divs=testDiv.find_all('div',class_='lightbox-caption')
-#gal0divs=testDiv.find_all('div',class_='wikia-gallery-item')
-for div in gal0divs:
-	print(div.span.get('data-param'))
-	print(div.span.a.get('href'))
-	#print(div.find('div',class_='lightbox-caption'))
 
-print('done')
+
+#reload(PrimeItem)
+def GetPrimeList():
+	source = urllib.request.urlopen('https://warframe.fandom.com/wiki/Prime').read()
+	soup=BS.BeautifulSoup(source,'html.parser')
+	dictOfGalleries={'gallery-0':'Warframe','gallery-1':'Primary','gallery-2':'Secondary','gallery-3':'Melee','gallery-4':'Comanion','gallery-6':'Archwing'}
+	dictionaryOfBlueprints={}
+	for key in dictOfGalleries:
+		galleriesBlueprints=[]
+		galleryDiv=soup.find('div',id=key)
+		divs=galleryDiv.find_all('div',class_='lightbox-caption')
+		if (key != 'gallery-4' and key != 'gallery-6'): #Companion and Archwing sections structured differently
+			for div in divs:
+				u=div.span.a.get('href')
+				bp=PrimeBlueprint(div.span.get('data-param'), url=u)
+				bp=FillBlueprint(bp)
+				galleriesBlueprints.append(bp)
+
+				#print(galleriesBlueprints.pop())
+
+				#print(div.span.get('data-param'))
+		else:
+			for div in divs:
+				galleriesBlueprints.append(PrimeBlueprint(div.a.get('title')))
+
+				#print(div.a.get('title'))
+		dictionaryOfBlueprints[dictOfGalleries[key]]=galleriesBlueprints
+	#testDiv=soup.find('div', id='gallery-0')
+
+	#gal0divs=testDiv.find_all('div',class_='lightbox-caption')
+	##gal0divs=testDiv.find_all('div',class_='wikia-gallery-item')
+	#for div in gal0divs:
+	#	print(div.span.get('data-param'))
+	#	print(div.span.a.get('href'))
+	#	#print(div.find('div',class_='lightbox-caption'))
+	#print(dictionaryOfBlueprints)
+
+	return dictionaryOfBlueprints
+
+def FillBlueprint(blueprint:PrimeBlueprint):
+	source=urllib.request.urlopen(blueprint.getURL())
+	bpSoup=BS.BeautifulSoup(source,'html.parser')
+	bpTable=bpSoup.find('table', class_='foundrytable')
+	#try:
+	if(bpTable):
+		element=bpTable.find_all('tr')
+		tdList=element[1].find_all('td')
+		#listOfParts=[]
+		i=0
+		while i<5:
+			if(tdList[i].a):
+				try:
+					amount=int(tdList[i].text.replace(',',''))
+				except:
+					amount=1
+
+				blueprint.AddPart(PrimePart(tdList[i].a.get('title'),amount))
+				#listOfParts.append(tdList[i].a.get('title'))
+				# check contents for number of items
+
+			i+=1
+		#listOfParts.append(tdList[1].a.get('title'))
+		#listOfParts.append(tdList[2].a.get('title'))
+		#listOfParts.append(tdList[3].a.get('title'))
+		#print(blueprint.name+': '+str(listOfParts))
+		#print(blueprint.listOfParts)
+	return blueprint
+		#print('Not creatable in foundery')
+	#except:
+	#	print('Not creatable in the foundery, source is :'+blueprint.getURL())
+
+
+
+def windowCreation():
+	window=Tk()
+	window.title('Prime List')
+	window.geometry('900x900')
+	TAB_CONTROL=ttk.Notebook(window)
+	TAB1=ttk.Frame(TAB_CONTROL)
+	TAB_CONTROL.add(TAB1,text='Warframes')
+
+	TAB2=ttk.Frame(TAB_CONTROL)
+	TAB_CONTROL.add(TAB2,text='Primaries')
+	TAB_CONTROL.pack(expand=1, fill='both')
+	ttk.Label(TAB1, text='Warframes go here').grid(column=0,row=0,padx=10,pady=10)
+	ttk.Label(TAB2, text='Primary Weapons go here').grid(column=0,row=0,padx=10,pady=10)
+	window.mainloop()
+
+#ListToPrint=GetPrimeList()
+windowCreation()
+
--- PrimeItem.py
+++ PrimeItem.py
@@ -1,36 +1,47 @@
 class PrimeBlueprint:
 	"""Blueprint of Prime Item. Holds references to the required parts"""
 	
-	def __init__(self, name, url='NO',vaulted=False, listOfParts=[], tags=[]):
+	def __init__(self, name, url='NO',vaulted=False, listOfParts=None, tags=[]):
 		self.name=name
 		if (url=='NO'):
-			url='https://warframe.fandom.com/wiki/'+str(name).replace(' ','_')
-		self.url=url
+			url=str(name).replace(' ','_')
+		self.url='https://warframe.fandom.com'+url
 		self.vaulted=vaulted
-		self.listOfParts=listOfParts
+		if listOfParts is None:
+			self.listOfParts=[]
+		else:
+			self.listOfParts=listOfParts
 		self.tags=[]
 
 	def __str__(self):
 		return self.name
 
-	def __repl__(self):
+	def __repr__(self):
 		return self.name
 
+	def getURL(self):
+		return self.url
+
+	def AddPart(self, part):
+		self.listOfParts.append(part)
 
 
 
 class PrimePart:
 	"""Parts of Primes """
 	
-	def __intit__(self, name, listOfParts=[]):
+	def __init__(self, name, amount=1, listOfParts=[]):
 		self.name=name
+		self.amount=amount
 		self.listOfParts=listOfParts
 
 	def __str__(self):
-		return self.name
+		return '<'+self.name+', '+str(self.amount)+'>'
 
-	def __repl__(self):
-		return self.name
+	def __repr__(self):
+		return str(self)
+
+
 
 
 
