3c419f776c2794db51d768379c44abb60832936b
Concrete18 <michaelericson19@gmail.com>
2020-03-24 21:07:43 -0400

    Updated code to PEP8

--- PrimeItem.py
+++ PrimeItem.py
@@ -1,17 +1,17 @@
 class PrimeBlueprint:
 	"""Blueprint of Prime Item. Holds references to the required parts"""
 	
-	def __init__(self, name, url='NO',vaulted=False, listOfParts=None, tags=[]):
-		self.name=name
-		if (url=='NO'):
-			url=str(name).replace(' ','_')
-		self.url='https://warframe.fandom.com'+url
-		self.vaulted=vaulted
+	def __init__(self, name, url='NO', vaulted=False, listOfParts=None, tags=[]):
+		self.name = name
+		if url == 'NO':
+			url = str(name).replace(' ', '_')
+		self.url = 'https://warframe.fandom.com'+url
+		self.vaulted = vaulted
 		if listOfParts is None:
-			self.listOfParts=[]
+			self.listOfParts = []
 		else:
-			self.listOfParts=listOfParts
-		self.tags=[]
+			self.listOfParts = listOfParts
+		self.tags = []
 
 	def __str__(self):
 		return self.name
@@ -26,31 +26,30 @@ class PrimeBlueprint:
 		self.listOfParts.append(part)
 
 	def JSONFormat(self):
-		jsonString=   '{"name":"'+self.name    +'",\n'
-		jsonString+=    '"url":"'+self.url     +'",\n'
-		#jsonString+='"vaulted":"'+self.vaulted +'",\n'
-		jsonString+='"listOfParts":['
-		first=True
+		jsonString = '{"name":"'+self.name + '",\n'
+		jsonString += '"url":"'+self.url + '",\n'
+		# jsonString+='"vaulted":"'+self.vaulted +'",\n'
+		jsonString += '"listOfParts":['
+		first = True
 		for part in self.listOfParts:
 			if first:
-				jsonString+=part.JSONFormat()
-				first=False
+				jsonString += part.JSONFormat()
+				first = False
 			else:
-				jsonString+=',\n'+part.JSONFormat()
-		jsonString=jsonString+']'
-		jsonString+='}'
+				jsonString += ',\n'+part.JSONFormat()
+		jsonString = jsonString+']'
+		jsonString += '}'
 
 		return jsonString
-
 
 
 class PrimePart:
 	"""Parts of Primes """
 	
 	def __init__(self, name, amount=1, listOfParts=[]):
-		self.name=name
-		self.amount=amount
-		self.listOfParts=listOfParts
+		self.name = name
+		self.amount = amount
+		self.listOfParts = listOfParts
 
 	def __str__(self):
 		return '<'+self.name+', '+str(self.amount)+'>'
@@ -59,16 +58,9 @@ class PrimePart:
 		return str(self)
 
 	def JSONFormat(self):
-		jsonString='{"partName":"'+self.name+'",'
-		jsonString+='"amount":'+str(self.amount)+''
+		jsonString = '{"partName":"'+self.name+'",'
+		jsonString += '"amount":'+str(self.amount)+''
 
-		jsonString+='}'
+		jsonString += '}'
 
 		return jsonString
-
-
-
-
-
-
-
